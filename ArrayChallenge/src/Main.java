ea9ff58de33b2268ecf9ba0aec133ba9bb22c240
KripaVS <kripa.ev@inapp.com>
2022-10-13 17:26:13 +0530

    new change

--- ArrayChallenge/src/Main.java
+++ ArrayChallenge/src/Main.java
@@ -5,6 +5,14 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Enter the size of the array:");
         int size=scanner.nextInt();
-        //int[] intArray=getInteger();
+        int[] intArray=getInteger(size);
+    }
+    public static int[] getInteger(int size){
+        System.out.println("Enter "+size+ "values\r");
+        int[] values=new int[size];
+        for(int i=0;i<values.length;i++){
+            values[i]=scanner.nextInt();
+        }
+        return values;
     }
 }
